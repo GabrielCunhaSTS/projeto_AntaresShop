49d33409271046bfe01405839dfeee37b4b23a0c
GabrielCunhaSTS <163028691+GabrielCunhaSTS@users.noreply.github.com>
2024-08-23 19:50:55 -0300

    23.08.24 - 19:50
    ° inicio do roteamento do servidor

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const { conecSequelize, banco } = require("./config/bdConnection");
+const routes = require('./src/routes/index')
+const path = require('path')
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
 
 conecSequelize.authenticate().then(() => {
     console.log(`----- conexão com o banco ${banco} estabelecida com sucesso! -----`);
@@ -14,3 +19,12 @@ const porta = 3000
 app.listen(porta, ()=> {
     console.log(`servidor rodando na porta ${porta}`)
 })  
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(cookieParser())
+app.use('/', routes)
+app.use(express.static('./src/views/'));
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'src','views'))
+
